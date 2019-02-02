76b902d99a2817d0b2e593f360e33f3d47b1a7ce
gerardward3 <gerardward3@gmail.com>
2019-02-02 21:55:53 +0000

    Adds evolve function.

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,20 +1,22 @@
 import pygame
 
+# global variables
 SCREEN_HEIGHT = 630
 SCREEN_WIDTH = 630
 
 GRID_HEIGHT = 25
 GRID_WIDTH = 25
 
-MARGIN = 5
 CELL_HEIGHT = 20
 CELL_WIDTH = 20
+MARGIN = 5
 
+# colour variables
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+# setup pygame
 pygame.init()
-
 screen = pygame.display.set_mode([SCREEN_HEIGHT, SCREEN_WIDTH])
     
 # creates grid
@@ -24,9 +26,9 @@ def create_grid():
         grid.append([])
         for column in range(GRID_WIDTH):
             grid[row].append(0)
-    grid[10][10] = 1
     return grid
 
+# draws grid on screen
 def draw_grid(grid):
     for row in range(GRID_WIDTH):
         for column in range(GRID_HEIGHT):
@@ -37,12 +39,43 @@ def draw_grid(grid):
                                               (MARGIN + CELL_HEIGHT) * row + MARGIN,
                                                CELL_WIDTH,
                                                CELL_HEIGHT])
-                                              
+
+# carries out iteration of game
+def evolve(grid):
+    next_grid = create_grid()
+    for row in range(GRID_HEIGHT):
+        for column in range(GRID_WIDTH):
+            cell = grid[row][column]
+            neighbours = count_neighbours(grid, row, column)
+            if cell == 0:
+                if neighbours === 3:
+                    cell = 1
+            else:
+                if neighbours < 2 or neighbours > 3:
+                    cell = 0
+            next_grid[row][column] = cell
+    return next_grid           
+
+# counts neighbour cells of a cell    
+# def count_neighbours(grid, x, y):
+
+    
 def main():
+    done = False
+    clock = pygame.time.Clock()
     grid = create_grid()
-    screen.fill(BLACK)
-    draw_grid(grid)
-    pygame.display.flip()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                
+        grid = evolve(grid)
+        screen.fill(BLACK)
+        draw_grid(grid)
+        clock.tick(20)
+        pygame.display.flip()
+        
+    pygame.quit()
     
 if __name__ == '__main__':
     main()
